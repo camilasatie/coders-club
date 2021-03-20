64d7134a2117bd21d286f63e41795aa365487861
Camila <camilasatie.dev@gmail.com>
2021-03-20 17:10:02 -0300

    add redux saga

--- redux-saga-typescript/src/App.tsx
+++ redux-saga-typescript/src/App.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 import SignIn from './pages/SignIn/SignIn';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <SignIn />
-    </>
+    </Provider>
   );
 }
 
--- redux-saga-typescript/src/pages/SignIn/SignIn.tsx
+++ redux-saga-typescript/src/pages/SignIn/SignIn.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { StoreState } from '../../store/createStore';
+import { signInRequest } from '../../store/modules/auth/actions';
 
 import scss from './SignIn.module.scss';
 
 import iconUser from '../../assets/login.svg';
 
 const SignIn: React.FC = () => {
+  const { loadingSignInRequest, isSignedIn, error, token } = useSelector((state: StoreState) => state.auth);
+  const dispatch = useDispatch();
+
+  console.log('LOADING:', loadingSignInRequest);
+  console.log('SIGN IN:', isSignedIn);
+  console.log('TOKEN:', token);
+  console.log('ERROR:', error);
+
   return (
     <div className={scss.container}>
       <img src={iconUser} className={scss.logo} alt="User Icon" />
       <input className={scss.input} type="text" defaultValue="test@email.com" />
       <input className={scss.input} type="password" defaultValue="123456" />
-      <button className={scss.btn} type="button" onClick={() => {}}>Entrar</button>
+      <button 
+        className={scss.btn} 
+        type="button" 
+        onClick={() => dispatch(signInRequest({ 
+          email: 'test@email.com', 
+          password: '123456' 
+        }))}
+      >
+        {loadingSignInRequest ? 'Carregando...' : 'Entrar'}
+      </button>
     </div>
   );
 }
